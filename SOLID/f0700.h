0e03799aa2ea31ad9001622eda31d89fe869ef1c
Joe <ProGamerCode@outlook.com>
2018-10-16 03:42:35 -0700

    Commented out removable code
    see title

--- SOLID/f0700.h
+++ SOLID/f0700.h
@@ -41,7 +41,7 @@
 // ********************************************************************
 
 
-signed int __cdecl f0700(int a1, int *a2, int a3, int *a4)
+signed int __cdecl f0700(const int a1, int a2[], const int a3, int *a4[])
 {
 	// Pre-Condition:
 	//    a1: possible input value = [64, 128, 256]
@@ -54,35 +54,41 @@ signed int __cdecl f0700(int a1, int *a2, int a3, int *a4)
 	// Post-Condition:
 	//    a1: value shouldn't change
 	//    a2: value shouldn't change
-	//    a3: value shouldn't changed
-	//    a4: value can changed
+	//    a3: value shouldn't change
+	//    a4: value can change
 
 
 	int *v4;
-	int v5;
+	
 	int v6;
-	int v7;
-	int o1;
+	
+	int result;
 	int v9;
 	int *v10;
 	int v11;
 	int v12;
 	int v13;
-	int v14;
-	int v15;
-	int a2a;
+	//not needed variables
+	//int v5;
+	//int v7;
+	//int v14;
+	//int v15;
+	//int a2a;
 
+	
+	//not needed placeholders
+	//v5 = a1;
+	//v7 = a2[a1];
+	//v14 = a2[a1 - 1];
+	//v15 = v7;
 	v4 = a2;
-	v5 = a1;
 	v6 = 0;
-	v7 = a2[a1];
-	v14 = a2[a1 - 1];
-	o1 = 0;
+	result = 0;
 	v9 = -1;
 	a2[a1 - 1] = 10000;
-	v15 = v7;
+
 	a2[a1] = -10000;
-	a2a = 0;
+	
 
 	if (a1 > 0)
 	{
@@ -102,18 +108,23 @@ signed int __cdecl f0700(int a1, int *a2, int a3, int *a4)
 					v12 += 4;
 					++v6;
 				} while (v9 * (*(int *)*v10 - v13) < a3);
-				v5 = a1;
+				//a1 is const
+				//v5 = a1;
 			}
 			++v10;
 			v9 = -v9;
-			o1 = a2a++ + 1;
-		} while (v6 < v5);
+			result = result + 2;
+		} while (v6 < a1);
 	}
-	if (o1 % 2)
-		--o1;
+	//if result is even decrement it by 1
+	if (result % 2)
+		--result;
+	/*
+	//omit unneeded assignments
 	v4[v5 - 1] = v14;
 	v4[v5] = v15;
-	return o1;
+	*/
+	return result;
 }
 
 #endif
